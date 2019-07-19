291d0041f2b5c2da61e48f9de7abf1ae22762678
Stephen Sherratt <stephen@sherra.tt>
2019-07-19 08:12:24 +0100

    Event handlers

--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,7 @@ use std::cell::RefCell;
 use std::rc::Rc;
 use wasm_bindgen::prelude::*;
 use wasm_bindgen::JsCast;
-use web_sys::HtmlElement;
+use web_sys::{HtmlElement, KeyboardEvent, MouseEvent, WheelEvent};
 
 #[wasm_bindgen]
 extern "C" {
@@ -26,6 +26,51 @@ pub fn run() -> Result<(), JsValue> {
         .unwrap()
         .set_inner_text("Hello, World!");
     let mut count = 0;
+    let handle_keydown = Closure::wrap(Box::new(move |event: JsValue| {
+        let keyboard_event = event.unchecked_ref::<KeyboardEvent>();
+        console_log!(
+            "keyboard event {} {}",
+            keyboard_event.key_code(),
+            keyboard_event.shift_key()
+        );
+    }) as Box<dyn FnMut(JsValue)>);
+    window
+        .add_event_listener_with_callback("keydown", handle_keydown.as_ref().unchecked_ref())
+        .unwrap();
+    handle_keydown.forget();
+    let handle_mouse = Closure::wrap(Box::new(move |event: JsValue| {
+        let mouse_event = event.unchecked_ref::<MouseEvent>();
+        console_log!(
+            "mouse event {} {} {}",
+            mouse_event.buttons(),
+            mouse_event.client_x(),
+            mouse_event.client_y(),
+        );
+    }) as Box<dyn FnMut(JsValue)>);
+    window
+        .add_event_listener_with_callback("mousemove", handle_mouse.as_ref().unchecked_ref())
+        .unwrap();
+    window
+        .add_event_listener_with_callback("mousedown", handle_mouse.as_ref().unchecked_ref())
+        .unwrap();
+    window
+        .add_event_listener_with_callback("mouseup", handle_mouse.as_ref().unchecked_ref())
+        .unwrap();
+    handle_mouse.forget();
+    let handle_wheel = Closure::wrap(Box::new(move |event: JsValue| {
+        let wheel_event = event.unchecked_ref::<WheelEvent>();
+        console_log!(
+            "wheel event {} {} {} {}",
+            wheel_event.delta_x(),
+            wheel_event.delta_y(),
+            wheel_event.client_x(),
+            wheel_event.client_y(),
+        );
+    }) as Box<dyn FnMut(JsValue)>);
+    window
+        .add_event_listener_with_callback("wheel", handle_wheel.as_ref().unchecked_ref())
+        .unwrap();
+    handle_wheel.forget();
     let f: Rc<RefCell<Option<Closure<_>>>> = Rc::new(RefCell::new(None));
     let g = f.clone();
     *g.borrow_mut() = Some(Closure::wrap(Box::new(move || {
